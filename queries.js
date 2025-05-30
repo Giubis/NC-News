815a7f0552223191b99b04a12e577f89fe8bcfce
Giubis <116124169+Giubis@users.noreply.github.com>
2025-05-30 12:01:54 +0100

    Task 4 completed

--- queries.js
+++ queries.js
@@ -3,7 +3,7 @@ const db = require("./db/connection");
 const getAllUsers = async () => {
   const queryResult = await db.query("SELECT * FROM users");
   const users = queryResult.rows.map((user) => user.username);
-  console.log(users);
+  // console.log(users);
   return users;
 };
 
@@ -12,9 +12,47 @@ const getAllArticlesByCodingTopic = async () => {
     "SELECT * FROM articles WHERE topic = 'coding'"
   );
   const articles = queryResult.rows.map((article) => article.title);
-  console.log(articles);
+  // console.log(articles);
   return articles;
+};
+
+const getAllCommentsWithNegativeRating = async () => {
+  const queryResult = await db.query(
+    "SELECT * FROM comments WHERE votes < '0'"
+  );
+  const comments = queryResult.rows.map((comment) => comment);
+  // console.log(comments);
+  return comments;
+};
+
+const getAllTopics = async () => {
+  const queryResult = await db.query("SELECT * FROM topics");
+  const topics = queryResult.rows.map((topic) => topic.slug);
+  // console.log(topics);
+  return topics;
+};
+
+const getAllArticlesByGrumpy19 = async () => {
+  const queryResult = await db.query(
+    "SELECT * FROM articles WHERE author = 'grumpy19'"
+  );
+  const articles = queryResult.rows.map((article) => article);
+  // console.log(articles);
+  return articles;
+};
+
+const getAllCommentsWithMoreTenVotes = async () => {
+  const queryResult = await db.query(
+    "SELECT * FROM comments WHERE votes > 10 OR votes < -10"
+  );
+  const comments = queryResult.rows.map((comment) => comment);
+  // console.log(comments);
+  return comments;
 };
 
 getAllUsers();
 getAllArticlesByCodingTopic();
+getAllCommentsWithNegativeRating();
+getAllTopics();
+getAllArticlesByGrumpy19();
+getAllCommentsWithMoreTenVotes();
